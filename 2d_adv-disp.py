1d06a72cd6903b62285c0bccb9820f9dfd36c6a9
Mehdi Samiee <samieeme@msu.edu>
2019-10-10 10:51:00 -0400

    2d-advection-dispersion
    advection-dispersion with constant coefficient

---
+++ 2d_adv-disp.py
@@ -0,0 +1,101 @@
+# In the name of Gd
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct  8 10:56:34 2019
+
+@author: samieeme
+"""
+
+import numpy as np
+#from functions import remain, deriv_x, deriv_y, deriv_z, div, Reduce_period, optimum_prnt
+#from Outputs import Output_Corr
+
+from functions_diff_2D import solV, forcef,  diff_eq, diff_x, adv_FE, adv_AB, solV_t, forcef_t, diff_FE, diff_AB,Ux_t,Uy_t
+
+Nnod = 10
+meshX = np.linspace(0,2*np.pi,Nnod+1)
+meshX = np.delete(meshX,Nnod,None)
+
+V = np.zeros([Nnod,Nnod])
+Ux = np.zeros([Nnod,Nnod])
+Uy = np.zeros([Nnod,Nnod])
+f = np.zeros([Nnod,Nnod])
+
+
+alpha_t = 10
+err = np.zeros([5])
+
+for kt in range(5):
+
+    dt = 0.001/5.0**(kt)
+    
+    for i in range(Nnod):
+        for j in range(Nnod):
+            V[i,j] = solV_t(meshX[i],meshX[j],(1-1)*dt)
+            Ux[i,j] = Ux_t(meshX[i],meshX[j],(1-1)*dt)
+            Uy[i,j] = Uy_t(meshX[i],meshX[j],(1-1)*dt)
+    #        f[i,j] = forcef(meshX[i],meshX[j])
+    
+    adv_velx = Ux[:] * V[:]
+    adv_vely = Uy[:] * V[:]
+    
+    
+    Vhat = np.fft.fft2(V)
+    Vhat_old = Vhat[:]
+    jmax = 10000
+    
+    for k1 in range(Nnod):
+        for k2 in range(Nnod):
+            f[k1,k2] = forcef_t(meshX[k1],meshX[k2],(1-1)*dt,alpha_t)    
+    fhat = np.fft.fft2(f)
+    adv_velx_hat = np.fft.fft2(adv_velx)
+    adv_vely_hat = np.fft.fft2(adv_vely)
+    adv_velx_hatold = adv_velx_hat[:]
+    adv_vely_hatold = adv_vely_hat[:]
+    #Vhat_new=diff_FE(Nnod,fhat,Vhat,alpha_t,dt)
+    Vhat_new = adv_FE(Nnod,fhat,Vhat,adv_velx_hat,adv_vely_hat,alpha_t,dt)
+    Vhat=Vhat_new
+    
+    for j in range(2,jmax):
+        fhat_old = fhat[:]
+        for k1 in range(Nnod):
+            for k2 in range(Nnod):
+                f[k1,k2] = forcef_t(meshX[k1],meshX[k2],(j-1)*dt,alpha_t)    
+        fhat = np.fft.fft2(f)
+        adv_velx_hat = Vhat[:] 
+        adv_vely_hat = Vhat[:]
+        adv_velx_hatold = Vhat_old[:] 
+        adv_vely_hatold = Vhat_old[:]
+       # Vhat_new=diff_AB(Nnod,fhat,fhat_old,Vhat,Vhat_old,alpha_t,dt)
+        #Vhat_new=adv_FE(Nnod,fhat,Vhat,adv_velx_hat,adv_vely_hat,alpha_t,dt)
+        Vhat_new=adv_AB(Nnod,fhat,fhat_old,Vhat,adv_velx_hat,adv_vely_hat,adv_velx_hatold,adv_vely_hatold,alpha_t,dt)
+        Vhat_old = Vhat[:]
+        Vhat=Vhat_new[:]
+        V = np.real(np.fft.ifft2(Vhat))
+        adv_velx = Ux[:] * V[:]
+        adv_vely = Uy[:] * V[:]
+        adv_velx_hatold = adv_velx_hat[:]
+        adv_vely_hatold = adv_vely_hat[:]
+
+        
+    V_new = np.real(np.fft.ifft2(Vhat_new))
+    for i in range(Nnod):
+        for j in range(Nnod):
+            V[i,j] = solV_t(meshX[i],meshX[j],(jmax-1)*dt)
+    
+    
+    #vs = diff_eq(Nnod,fhat)
+    #dxV = diff_x(Nnod,Vhat)
+    
+    err[kt] = abs(np.max(V_new[:] - V[:]))
+
+
+
+dt1 = 0.01
+dt2 = 0.01/5.0**5.
+rate_conver = (np.log(err[2])-np.log(err[0]))/(np.log(dt2)-np.log(dt1))
+
+
+#%%
+
+
--- functions_diff_2D.py
+++ functions_diff_2D.py
@@ -9,14 +9,23 @@ import numpy as np
 def forcef(x,y):
     return -4.0*np.sin(2.0*x)*np.cos(3.0*y)-9.0*np.sin(2.0*x)*np.cos(3.0*y)
 def forcef_t(x,y,t,alpha):
-#    return -2.0*solV_t(x,y,t)-alpha*(-4.0*np.sin(2.0*x)*np.cos(3.0*y)-9.0*np.sin(2.0*x)*np.cos(3.0*y))*np.exp(-2*t)
-    return np.sin(2.0*x)*np.cos(3.0*y)*2.5*t**(1.5)-alpha*t**(2.5)*(-4.0*np.sin(2.0*x)*np.cos(3.0*y)-9.0*np.sin(2.0*x)*np.cos(3.0*y)) #-10.0*solV_t(x,y,t)-alpha*(-4.0*np.sin(2.0*x))*np.exp(-10*t)
+    return np.sin(2.0*x)*np.cos(3.0*y)*2.5*t**(1.5)-alpha*t**(2.5)*(-4.0*np.sin(2.0*x)*np.cos(3.0*y)-9.0*np.sin(2.0*x)*np.cos(3.0*y))+Ux_t(x,y,t)*2.0*np.cos(2.0*x)*np.cos(3.0*y)*t**(2.5)-Uy_t(x,y,t)*3.0*np.sin(2.0*x)*np.sin(3.0*y)*t**(2.5)
+  #  return 2.0*np.cos(2.0*x)*np.cos(3.0*y)-3.0*np.sin(2.0*x)*np.sin(3.0*y)-alpha*(-4.0*np.sin(2.0*x)*np.cos(3.0*y)-9.0*np.sin(2.0*x)*np.cos(3.0*y))
 
 def solV(x,y):
     return np.sin(2.0*x)*np.cos(3.0*y)
 def solV_t(x,y,t):
 #    return np.sin(2.0*x)*np.cos(3.0*y)*np.exp(-2*t)
     return np.sin(2.0*x)*np.cos(3.0*y)*t**(2.5) #np.sin(2.0*x)*np.exp(-10*t)
+ #   return np.sin(2.0*x)*np.cos(3.0*y)
+
+
+def Ux_t(x,y,t):
+    return 1.0 #3.0*np.sin(2.0*x)*np.cos(3.0*y) #np.sin(2.0*x)*np.exp(-10*t)
+
+def Uy_t(x,y,t):
+    return 1.0 #2.0*np.cos(2.0*x)*np.sin(3.0*y) #np.sin(2.0*x)*np.exp(-10*t)
+
 
 def deriv_x(Nnod,Vhat):
     divhat = np.zeros((Nnod,Nnod,Nnod),dtype = complex)
@@ -70,7 +79,7 @@ def diff_eq(Nnod,fhat):
     diverz_V[0,0] = 0.0
     return diverz_V
 
-def adv_FE(Nnod,fhat,vhat,diffusivity,dt):
+def diff_FE(Nnod,fhat,vhat,diffusivity,dt):
     #frac_L = np.zeros((Nnod,Nnod))
     kx = np.zeros((Nnod,Nnod))
     ky = np.zeros((Nnod,Nnod))
@@ -96,7 +105,7 @@ def adv_FE(Nnod,fhat,vhat,diffusivity,dt):
     
     
     
-def adv_AB(Nnod,fhat,fhat_old,vhat,vhat_old,diffusivity,dt):
+def diff_AB(Nnod,fhat,fhat_old,vhat,vhat_old,diffusivity,dt):
     #frac_L = np.zeros((Nnod,Nnod))
     kx = np.zeros((Nnod,Nnod))
     ky = np.zeros((Nnod,Nnod))
@@ -119,9 +128,84 @@ def adv_AB(Nnod,fhat,fhat_old,vhat,vhat_old,diffusivity,dt):
     
     solution = operator_diff * vhat + 3.0/2.0*operator_force * fhat - 1.0/2.0*operator_force * fhat_old 
     return solution    
+
+
+def adv_FE(Nnod,fhat,vhat,adv_velx_hat,adv_vely_hat,diffusivity,dt):
+    #frac_L = np.zeros((Nnod,Nnod))
+    kxx = np.zeros((Nnod,Nnod))
+    kyy = np.zeros((Nnod,Nnod))
+   # kx[0,0]=1.0/10000000.0
+    for i1 in range(Nnod):
+        if i1 <= (Nnod-1)/2:
+            kxx[i1,:] = i1
+        else:
+            kxx[i1,:] = (i1-Nnod)
+    for i2 in range(Nnod):
+        if i2 <= (Nnod-1)/2:
+            kyy[:,i2] = i2
+        else:
+            kyy[:,i2] = (i2-Nnod)
     
+    kx = np.zeros((Nnod,Nnod),dtype=complex)
+    for i1 in range(Nnod):
+        if i1 <= (Nnod-1)/2:
+           kx[i1,:] = complex(0,i1)
+        else:
+           kx[i1,:] = complex(0,i1-Nnod)   
+    ky = np.zeros((Nnod,Nnod),dtype=complex)
+    for i2 in range(Nnod):
+        if i2 <= (Nnod-1)/2:
+           ky[:,i2] = complex(0,i2)
+        else:
+           ky[:,i2] = complex(0,i2-Nnod)                  
+    alpha = 1;
+    bb = 0.5
+    identity = np.ones((Nnod,Nnod))
+    operator_diff = (identity[:]-diffusivity*dt*(1.0-bb)*(kxx[:]**2+kyy[:]**2)**(alpha))/(identity[:]+diffusivity*dt*bb*(kxx[:]**2+kyy[:]**2)**(alpha))
+    operator_force = dt*(identity[:])/(identity[:]+diffusivity*dt*bb*(kxx[:]**2+kyy[:]**2)**(alpha))
+    operator_adv = -dt*(kx[:]*adv_velx_hat[:]+ky[:]*adv_vely_hat[:])/(identity[:]+diffusivity*dt*bb*(kxx[:]**2+kyy[:]**2)**(alpha))
     
+    solution = operator_diff * vhat + operator_force * fhat + 1.0*operator_adv
+    return solution    
     
+def adv_AB(Nnod,fhat,fhat_old,vhat,adv_velx_hat,adv_vely_hat,adv_velx_hatold,adv_vely_hatold,diffusivity,dt):
+    #frac_L = np.zeros((Nnod,Nnod))
+    kxx = np.zeros((Nnod,Nnod))
+    kyy = np.zeros((Nnod,Nnod))
+    #kx[0,0]=1.0/10000000.0
+    for i1 in range(Nnod):
+        if i1 <= (Nnod-1)/2:
+            kxx[i1,:] = i1
+        else:
+            kxx[i1,:] = (i1-Nnod)
+    for i2 in range(Nnod):
+        if i2 <= (Nnod-1)/2:
+            kyy[:,i2] = i2
+        else:
+            kyy[:,i2] = (i2-Nnod)
+    
+    kx = np.zeros((Nnod,Nnod),dtype=complex)
+    for i1 in range(Nnod):
+        if i1 <= (Nnod-1)/2:
+           kx[i1,:] = complex(0,i1)
+        else:
+           kx[i1,:] = complex(0,i1-Nnod)   
+    ky = np.zeros((Nnod,Nnod),dtype=complex)
+    for i2 in range(Nnod):
+        if i2 <= (Nnod-1)/2:
+           ky[:,i2] = complex(0,i2)
+        else:
+           ky[:,i2] = complex(0,i2-Nnod)    
+    alpha = 1;
+    bb = 0.5
+    identity = np.ones((Nnod,Nnod))
+    operator_diff = (identity[:]-diffusivity*dt*(1.0-bb)*(kxx[:]**2+kyy[:]**2)**(alpha))/(identity[:]+diffusivity*dt*bb*(kxx[:]**2+kyy[:]**2)**(alpha))
+    operator_force = dt*(identity[:])/(identity[:]+diffusivity*dt*bb*(kxx[:]**2+kyy[:]**2)**(alpha))
+    operator_adv = -dt*(kx[:]*adv_velx_hat[:]+ky[:]*adv_vely_hat[:])/(identity[:]+diffusivity*dt*bb*(kxx[:]**2+kyy[:]**2)**(alpha))
+    operator_adv_old = -dt*(kx[:]*adv_velx_hatold[:]+ky[:]*adv_vely_hatold[:])/(identity[:]+diffusivity*dt*bb*(kxx[:]**2+kyy[:]**2)**(alpha))
+    
+    solution = operator_diff * vhat + 3.0/2.0*operator_force * fhat - 1.0/2.0*operator_force * fhat_old + 3.0/2.0*operator_adv - 1.0/2.0*operator_adv_old
+    return solution 
     
     
     
